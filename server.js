caf7ab26ea98310aaa7c113d0dc1f74c40a04762
Adam And <andadam24@gmail.com>
2021-08-28 12:50:43 -0400

    working on having my get/post to work properly

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// Dependencies
-
+//Dependencies
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+//Set up server
 const app = express();
 const PORT = process.env.PORT || 3000;
 
